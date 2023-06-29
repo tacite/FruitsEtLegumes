9f4d6368baf7bdde9aad8b08e4b857702da2dea6
David Tacite <david.tacite@gmail.com>
2023-06-29 15:59:38 +0200

    Initial Commit

---
+++ FruitsEtLegumes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FruitsEtLegumes
+//
+//  Created by David Tacite on 29/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FruitsEtLegumes/FruitsEtLegumesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsEtLegumesApp.swift
+//  FruitsEtLegumes
+//
+//  Created by David Tacite on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsEtLegumesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
