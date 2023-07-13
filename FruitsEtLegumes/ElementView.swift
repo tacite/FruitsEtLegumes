4fbbc8f34721e970d6b2aa90b4115cf6b59cf4d3
David Tacite <david.tacite@gmail.com>
2023-07-13 15:28:30 +0200

    gros changements

---
+++ FruitsEtLegumes/ElementView.swift
@@ -0,0 +1,18 @@
+//
+//  ElementView.swift
+//  FruitsEtLegumes
+//
+//  Created by David Tacite on 29/06/2023.
+//
+
+import SwiftUI
+
+struct ElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ElementView()
+}
